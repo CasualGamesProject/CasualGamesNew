7ccdd840da1dbc0d676b62c0fec726aa64f276ca
S00162322-NoelConneely <31918638+s00162322@users.noreply.github.com>
2017-12-21 19:25:53 +0000

    New Casual Games project

---
+++ CasualGamesneu/MonoGameClient/Game1.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CommonDataItems;
+using System;
+using Sprites;
+using GameComponentNS;
+using System.Collections.Generic;
+
+namespace MonoGameClient
+{
+    
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+
+        string connectionMessage = string.Empty;
+
+
+        HubConnection serverConnection;
+        IHubProxy proxy;
+
+        public bool Connected { get; private set; }
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+       
+        protected override void Initialize()
+        {
+            serverConnection = new HubConnection("http://localhost:50983/");
+            serverConnection.StateChanged += ServerConnection_StateChanged;
+            proxy = serverConnection.CreateHubProxy("GameHub");
+            serverConnection.Start();
+
+            Action<PlayerData> joined = clientJoined;
+            proxy.On<PlayerData>("Joined", joined);
+
+            Action<List<PlayerData>> currentPlayers = clientPlayers;
+            proxy.On<List<PlayerData>>("CurrentPlayers", currentPlayers);
+
+            Action<string, Position> otherMove = clientOtherMoved;
+            proxy.On<string, Position>("OtherMove", otherMove);
+
+            base.Initialize();
+        }
+
+        private void clientOtherMoved(string playerID, Position newPos)
+        {
+            // iterate over all the other player components 
+            // and check to see the type and the right id
+            foreach (var player in Components)
+            {
+                if (player.GetType() == typeof(OtherPlayerSprite)
+                    && ((OtherPlayerSprite)player).pData.playerID == playerID)
+                {
+                    OtherPlayerSprite p = ((OtherPlayerSprite)player);
+                    p.pData.playerPosition = newPos;
+                    p.Position = new Point(p.pData.playerPosition.X, p.pData.playerPosition.Y);
+                    break; // break out of loop as only one player position is being updated
+                           // and we have found it
+                }
+            }
+        }
+
+
+        // Only called when the client joins a game
+        private void clientPlayers(List<PlayerData> otherPlayers)
+        {
+            foreach (PlayerData player in otherPlayers)
+            {
+                // Create an other player sprites in this client after
+                new OtherPlayerSprite(this, player, Content.Load<Texture2D>(player.imageName),
+                                        new Point(player.playerPosition.X, player.playerPosition.Y));
+                connectionMessage = player.playerID + " delivered ";
+            }
+        }
+
+        private void clientJoined(PlayerData otherPlayerData)
+        {
+            // Create an other player sprite
+            //use points in game to make movement smoother
+            new OtherPlayerSprite(this, otherPlayerData, Content.Load<Texture2D>(otherPlayerData.imageName),
+                                    new Point(otherPlayerData.playerPosition.X, otherPlayerData.playerPosition.Y));
+            new FadeText(this, Vector2.Zero, otherPlayerData.GamerTag + "Has joined the game");
+        }
+
+
+        /// <summary>
+        /// ////////////
+        /// </summary>
+
+
+        private void ServerConnection_StateChanged(StateChange State)
+        {
+            switch (State.NewState)
+            {
+                case ConnectionState.Connected:
+                    connectionMessage = "Connected......";
+                    Connected = true;
+                    startGame();
+                    break;
+                case ConnectionState.Disconnected:
+                    connectionMessage = "Disconnected.....";
+                    if (State.OldState == ConnectionState.Connected)
+                        connectionMessage = "Lost Connection....";
+                    Connected = false;
+                    break;
+                case ConnectionState.Connecting:
+                    connectionMessage = "Connecting.....";
+                    Connected = false;
+                    break;
+
+            }
+        }
+
+        private void startGame()
+        {
+            // Continue on and subscribe to the incoming messages joined, currentPlayers, otherMove messages
+
+            // Immediate Pattern
+            proxy.Invoke<PlayerData>("Join")
+                .ContinueWith( // This is an inline delegate pattern that processes the message 
+                               // returned from the async Invoke Call
+                        (p) => { // Wtih p do 
+                            if (p.Result == null)
+                                connectionMessage = "No player Data returned";
+                            else
+                            {
+                                CreatePlayer(p.Result);
+                                // Here we'll want to create our game player using the image name in the PlayerData 
+                                // Player Data packet to choose the image for the player
+                                // We'll use a simple sprite player for the purposes of demonstration 
+
+                            }
+
+                        });
+        }
+
+        private void CreatePlayer(PlayerData player)
+        {
+            new SimplePlayerSprite(this, player, Content.Load<Texture2D>(player.imageName),
+                new Point(player.playerPosition.X, player.playerPosition.Y));
+
+            new FadeText(this, Vector2.Zero, " Welcome " + player.GamerTag + " you are playing as " + player.imageName);
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Services.AddService<SpriteBatch>(spriteBatch);
+            font = Content.Load<SpriteFont>("Message");
+            Services.AddService<SpriteFont>(font);
+
+        }
+
+       
+      
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            base.Update(gameTime);
+        }
+
+       
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, connectionMessage, new Vector2(10, 10), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ CasualGamesneu/WebApplication1/GameHub.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using CommonDataItems;
+
+namespace WebApplication1
+{
+    public class GameHub : Hub
+    {
+        public static Queue<PlayerData> RegisteredPlayers = new Queue<PlayerData>(new PlayerData[]
+        {
+            new PlayerData {GamerTag = "Wengu", imageName = "", playerID = Guid.NewGuid().ToString(), Coins = 0 },
+            new PlayerData {GamerTag = "Crush", imageName = "", playerID = Guid.NewGuid().ToString(), Coins = 0 },
+            new PlayerData {GamerTag = "Hawke", imageName = "", playerID = Guid.NewGuid().ToString(), Coins = 0 },
+            new PlayerData {GamerTag = "Bazooka", imageName = "", playerID = Guid.NewGuid().ToString(), Coins = 0 },
+
+        });
+
+
+        public static List<PlayerData> Players = new List<PlayerData>();
+
+        public static Stack<string> characters = new Stack<string>(new string[] {"Player 4", "Player 3", "Player 2", "Player 1" });
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+
+
+        public PlayerData Join()
+        {
+            // Check and if the charcters
+            if (characters.Count > 0)
+            {
+                // pop name
+                string character = characters.Pop();
+                // if there is a registered player
+                if (RegisteredPlayers.Count > 0)
+                {
+                    PlayerData newPlayer = RegisteredPlayers.Dequeue();
+                    newPlayer.imageName = character;
+                    newPlayer.playerPosition = new Position
+                    {
+                        X = new Random().Next(700),
+                        Y = new Random().Next(500)
+                    };
+                    // Tell all the other clients that this player has Joined
+                    Clients.Others.Joined(newPlayer);
+                    // Tell this client about all the other current 
+                    Clients.Caller.CurrentPlayers(Players);
+                    // add the new player on the server
+                    Players.Add(newPlayer);
+                    return newPlayer;
+                }
+
+
+            }
+            return null;
+        }
+
+
+        public void Moved(string playerID, Position newPosition)
+        {
+            // Update the collection with the new player position is the player exists
+            PlayerData found = Players.FirstOrDefault(p => p.playerID == playerID);
+
+            if (found != null)
+            {
+                // Update the server player position
+                found.playerPosition = newPosition;
+                // Tell all the other clients this player has moved
+                Clients.Others.OtherMove(playerID, newPosition);
+            }
+        }
+    }
+}
