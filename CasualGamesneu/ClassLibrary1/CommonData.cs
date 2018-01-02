4bffcd35e8895b6d27865ff79b3d68a9eeb3299b
Peter Casey <s00162137@mail.itsligo.ie>
2018-01-02 13:19:56 +0000

    Coin Class

--- CasualGamesneu/ClassLibrary1/CommonData.cs
+++ CasualGamesneu/ClassLibrary1/CommonData.cs
@@ -23,4 +23,12 @@ namespace CommonDataItems
         public Position playerPosition;
         public string Password;
     }
+
+    public class CoinData
+    {
+        public string imageName = string.Empty;
+        public Position coinPosition;
+        
+    }
+
 }
--- CasualGamesneu/WebApplication1/GameHub.cs
+++ CasualGamesneu/WebApplication1/GameHub.cs
@@ -18,6 +18,7 @@ namespace WebApplication1
 
         });
 
+        public static List<CoinData>
 
         public static List<PlayerData> Players = new List<PlayerData>();
 
